651fd25d6e51ecd541e253ad03c6e8f28a6ae3d1
tablonimus <lautamartin1995@gmail.com>
2022-12-23 12:44:24 -0300

    ahora si

--- src/components/NavBar/NavResults.jsx
+++ src/components/NavBar/NavResults.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useSelector } from "react-redux";
 import { Spinner } from "flowbite-react";
+import MatchResult from "./MatchResult";
 
 export default function NavResults() {
   const matches = useSelector((state) => state.allMatches);
@@ -11,56 +12,19 @@ export default function NavResults() {
       <div className="flex items-center overflow-x-auto  lg:justify-center  w-full">
         {matches.length > 0 ? (
           matches.map((match) => (
-            <section className="" key={match.id}>
-              <div className=" m-2 h-24 w-36 border border-[#E96F22] shadow-inner shadow-white rounded-lg">
-                <span className="flex flex-row items-center m-1 justify-between">
-                  <div className="flex flex-col items-center justify-center">
-                    <img
-                      src={match?.Teams[0]?.logo}
-                      alt="local"
-                      className="w-6 h-6 rounded-full bg-white"
-                    />
-                    <h5 className="text-white font-bold text-[10px]">
-                      {match?.Teams[0]?.short_name}
-                    </h5>
-                  </div>
-                  <h5 className="text-white font-bold text-[13px]">
-                    {match.local_score}
-                  </h5>
-                  <h5 className="text-white font-bold text-[13px]">
-                    {match.visitor_score}
-                  </h5>
-                  <div className="flex flex-col items-center justify-center">
-                    <img
-                      src={match?.Teams[1]?.logo}
-                      alt="visitor"
-                      className="w-6 h-6 rounded-full bg-white"
-                    />
-                    <h5 className="text-white font-bold text-[10px]">
-                      {match?.Teams[1]?.short_name}
-                    </h5>
-                  </div>
-                </span>
-
-                <div className="border-t border-[#E96F22] flex justify-center m-1">
-                  {match.finished === false ? (
-                    <h5 className="text-green-300 font-bold text-[10px] ">
-                      {" "}
-                      {match.time} - {match.date.split("-").reverse().join("/")}
-                    </h5>
-                  ) : (
-                    <h5 className="text-red-300 font-bold text-[10px] ">
-                      Partido terminado
-                    </h5>
-                  )}
-                </div>
-                <div className="border-t flex justify-center m-1">
-                  <h5 className="text-white font-bold text-[10px] ">
-                    Cancha: {match.place}
-                  </h5>
-                </div>
-              </div>
-            </section>
+            <MatchResult
+              key={match.id}
+              id={match.id}
+              local={match.local}
+              local_score={match.local_score}
+              visitor={match.visitor}
+              visitor_score={match.visitor_score}
+              time={match.time}
+              date={match.date}
+              place={match.place}
+              finished={match.finished}
+              teams = {match.Teams}
+            />
           ))
         ) : (
           <div className="ml-10 flex items-center justify-center gap-5">
