c5f19058b76a07b3f2b670d47fa4bbd8991fef4c
tablonimus <lautamartin1995@gmail.com>
2023-07-14 20:31:43 -0300

    f1 solved

--- src/components/NavBar/NavTwo.jsx
+++ src/components/NavBar/NavTwo.jsx
@@ -43,7 +43,7 @@ export default function NavTwo() {
     <div className="bg-black  w-full  flex flex-col items-center mt-24 py-5 m-2">
       <h1 className="font-bold text-[#E96F22] lg:text-3xl mb-5">
      
-        {category === "A1" || !category ? "#A1xSuela" :category === "F1"?
+        {category === "A1" || !category ? "#A1xSuela" :category === "FEM"?
          "#FemxSuela":
          "#DHxSuela"}
       </h1>
