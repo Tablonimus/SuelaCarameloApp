b586547af8aa433fb352774a80fce6148573175a
Agustin Morro <agustin.morro@gmail.com>
2024-03-16 00:38:11 -0300

    add title club

--- src/components/Clubs/Clubs.jsx
+++ src/components/Clubs/Clubs.jsx
@@ -14,9 +14,25 @@ import clubsFem from "../../utils/data/teamsFem.json";
 import { Link } from "react-router-dom";
 
 export default function Clubs() {
-  
   return (
     <>
+      <article className="my-5 flex flex-col justify-center items-center text-center lg:my-12 ">
+        <h1 className="text-4xl font-extrabold text-zinc-800 leading-snug lg:text-6xl">
+          Suela Caramelo
+          <br />
+          <span className="px-1 italic text-black">Clubes</span>
+        </h1>
+        <p className="text-md font-semibold text-zinc-900 mt-7 lg:text-lg">
+          Lorem, ipsum dolor sit amet consectetur adipisicing. 
+          <br />
+          Lorem ipsum dolor sit amet.
+        </p>
+        <button
+          className="bg-zinc-900 w-56 text-gray-300 py-2 rounded-md font-bold text-sm mt-4 hover:scale-105 hover:text-[#ED7020] duration-200 lg:mt-6"
+        >
+          Contact me
+        </button> 
+      </article>
       <section className="flex flex-col gap-4 p-4">
         <h1 className="text-center text-4xl text-white font-bold">Masculino</h1>
         <div className="flex flex-wrap justify-center gap-2 sm:gap-8">
@@ -25,7 +41,7 @@ export default function Clubs() {
                 <Link key={index} to={`/clubes/${club.name}`}>
                   <div
                     key={club.name}
-                    className="flex text-center items-center justify-center overflow-hidden w-32 px-0 py-3 sm:w-64 lg:p-6 rounded-lg  bg-[#0a1b21]/80  "
+                    className="flex text-center items-center justify-center overflow-hidden w-32 px-0 py-3 sm:w-64 lg:p-6 rounded-lg  bg-[#0a1b21]/80 hover:scale-105 duration-200"
                   >
                     <section>
                       <img
