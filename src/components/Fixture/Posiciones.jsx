d3c7fc7088315b8c2e30759d2b32e83c60180263
Agustin Morro <agustin.morro@gmail.com>
2024-04-08 12:05:31 -0300

    fix btns notice-posiciones

--- src/components/Fixture/Posiciones.jsx
+++ src/components/Fixture/Posiciones.jsx
@@ -10,23 +10,25 @@ import { useState } from "react";
 const Posiciones = () => {
   const [positionState, setPositionState] = useState("A1");
 
+
+
   return (
     <>
       <Sidebar />
-      <section className="pl-[70px] w-full flex flex-col justify-center items-center bg-zinc-900 gap-2 py-6">
-        <h2 className="text-2xl  text-white font-bold">POSICIONES</h2>
-        <picture className="flex justify-center items-center overflow-hidden gap-3 lg:px-8 lg:gap-8">
+      <section className="pl-[70px] w-full flex flex-col justify-center items-center bg-zinc-900 gap-2 py-4 lg:py-6">
+        <h2 className="text-xl mb-2 italic lg:mb-4 lg:text-2xl  text-gray-200 font-bold">POSICIONES</h2>
+        <picture className="flex justify-center items-center overflow-hidden gap-8 lg:gap-12">
           <img
             src={logoA1}
             name="A1"
-            className="w-16 h-16  cursor-pointer hover:scale-105 duration-300 object-cover"
+            className={`w-12  ${positionState === "A1" ? "scale-110" : null } cursor-pointer hover:scale-105 duration-300 object-cover lg:w-16`}
             alt=""
             onClick={(e) => setPositionState(e.target.name)}
           />
           <img
             src={logoF1}
             name="FEM"
-            className="w-16 h-16 cursor-pointer hover:scale-110 duration-300 object-cover"
+            className={`w-12 cursor-pointer ${positionState === "FEM" ? "scale-110" : null } hover:scale-110 duration-300 object-cover lg:w-16`}
             alt=""
             onClick={(e) => setPositionState(e.target.name)}
           />
--- src/components/Home/Noticias.jsx
+++ src/components/Home/Noticias.jsx
@@ -15,12 +15,13 @@ import logoTN from "../../assets/images/botones/TN.webp";
 export default function Noticias() {
   const dispatch = useDispatch();
   const [noticeState, setNoticeState] = useState("A1xSuela");
+  console.log(noticeState)
   const jsonNotices = useSelector((state) =>
     noticesJson
       .sort((a, b) => {
-        return a.id - b.id;
+        return b.id - a.id;
       })
-      .reverse()
+     
   );
 
   const [allNotices, setAllNotices] = useState(jsonNotices);
@@ -29,12 +30,12 @@ export default function Noticias() {
     let notices = jsonNotices.filter(
       (notice) => e.target.name === notice.category
     );
-
+console.log(e.target.name)
     setAllNotices(notices);
-    if (e.target.value === "A1") setNoticeState("A1xSuela");
-    if (e.target.value === "FEM") setNoticeState("F1xSuela");
-    if (e.target.value === "TI") setNoticeState("Torneos Internacionales");
-    if (e.target.value === "TN") setNoticeState("Torneos Nacionales");
+    if (e.target.name === "A1") setNoticeState("A1xSuela");
+    if (e.target.name === "FEM") setNoticeState("F1xSuela");
+    if (e.target.name === "TI") setNoticeState("Torneos Internacionales");
+    if (e.target.name === "TN") setNoticeState("Torneos Nacionales");
   };
 
   //PAGINATION---
@@ -53,35 +54,36 @@ export default function Noticias() {
   const totalPages = Math.ceil(allNotices.length / noticesPerPage);
 
   return (
-    <div className="pl-[70px] flex flex-col w-full lg:h-screen justify-between items-center">
-      <section className="w-full flex flex-col justify-center items-center bg-zinc-900 gap-2 py-6">
-        <h2 className="text-2xl text-white font-bold">NOTICIAS</h2>
-        <picture className="flex justify-center items-center overflow-hidden gap-3 lg:px-8 lg:gap-8">
+    <div className="pl-[70px]  flex flex-col w-full justify-between items-center">
+      <section className="w-full flex flex-col justify-center items-center bg-zinc-900 gap-2 py-4 lg:py-6">
+        <h2 className="text-xl mb-2 italic lg:mb-4 lg:text-2xl  text-gray-200 font-bold">POSICIONES</h2>
+        <picture className="flex justify-center items-center overflow-hidden gap-6 lg:px-8 lg:gap-10">
           <img
+            style={{}}
             src={logoA1}
             name="A1"
-            className="w-16 h-16  cursor-pointer hover:scale-105 duration-300 object-cover"
+            className={`w-12 lg:w-16 ${noticeState === "A1xSuela" ? "scale-110" : null }  cursor-pointer hover:scale-105 duration-300 object-cover`}
             alt=""
             onClick={(e) => categoryHandler(e)}
           />
           <img
             src={logoF1}
             name="FEM"
-            className="w-16 h-16 cursor-pointer hover:scale-110 duration-300 object-cover"
+            className={`w-12 lg:w-16 ${noticeState === "F1xSuela" ? "scale-110" : null }  cursor-pointer hover:scale-105 duration-300 object-cover`}
             alt=""
             onClick={(e) => categoryHandler(e)}
           />
           <img
             src={logoTN}
             name="TN"
-            className="w-14 h-14 cursor-pointer hover:scale-110 duration-300 object-cover"
+            className={`w-[45px] lg:w-14 ${noticeState === "Torneos Nacionales" ? "scale-110" : null }  cursor-pointer hover:scale-105 duration-300 object-cover`}
             alt=""
             onClick={(e) => categoryHandler(e)}
           />
           <img
             src={logoTI}
             name="TI"
-            className="w-14 h-14 cursor-pointer hover:scale-110 duration-300 object-cover"
+            className={`w-[45px] lg:w-14 ${noticeState === "Torneos Internacionales" ? "scale-110" : null }  cursor-pointer hover:scale-105 duration-300 object-cover`}
             alt=""
             onClick={(e) => categoryHandler(e)}
           />
